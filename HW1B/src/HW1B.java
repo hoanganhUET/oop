5b13031b2be30b3a0260b322d85187c733ff0d0b
hoanganhUET <163994097+hoanganhUET@users.noreply.github.com>
2024-04-10 11:50:12 +0700

    apr10

---
+++ HW1B/src/HW1B.java
@@ -0,0 +1,9 @@
+public class HW1B {
+    public static void main(String[] args) {
+        System.out.println("Ho va ten: Do Hoang Anh\tMSSV: 23020325\tLop: K68AI1\tGithub: hoanganhUET");
+        for (int i=19;i>0;--i){
+            System.out.println(i+" bottles of beer on the wall" + i + " bottles of beer.\n" + "Take one down, pass it around,\n ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
