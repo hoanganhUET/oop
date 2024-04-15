87f40888339996aba9f3b8ddf724819bef5cc3cd
hoanganhUET <163994097+hoanganhUET@users.noreply.github.com>
2024-04-15 22:39:14 +0700

    apr152024

---
+++ Factors/src/Factors.java
@@ -0,0 +1,19 @@
+public class Factors {
+
+    public static void main(String[] args) {
+
+        long n = Long.parseLong(args[0]);
+
+        System.out.print("The prime factorization of " + n + " is: ");
+
+        for (long factor = 2; factor*factor <= n; factor++) {
+
+            while (n % factor == 0) {
+                System.out.print(factor + " ");
+                n = n / factor;
+            }
+        }
+        if (n > 1) System.out.println(n);
+        else       System.out.println();
+    }
+}
